79c81f89014abe3108e57da38bdd3b7902a4fb28
https-Myrtle <myrtleargote5@gmail.com>
2026-07-06 05:09:03 +0800

    Add files via upload

---
+++ MPG = miles and gallon.py
@@ -0,0 +1,7 @@
+miles = input('Enter the miles driven:')
+
+gallon = input('Enter the gallons of fuel used:')
+
+result = float(miles) / float(gallon)
+
+print('You used ', round(result, 2), 'miles per gallon. ')
