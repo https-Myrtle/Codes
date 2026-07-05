9eb1033ffee271bdfe2f84065c7b009318a2299f
https-Myrtle <myrtleargote5@gmail.com>
2026-07-06 05:54:17 +0800

    Update Land = Acres and Sq Ft.py

--- Land = Acres and Sq Ft.py
+++ Land = Acres and Sq Ft.py
@@ -1,6 +1,14 @@
+#Author Myrtle Abigail
+#Date 07/06/2026
+
+'''
+input the number of sq ft in the tract
+
+result = ft / 43560
+
+display the size of the tract  #rounded by 2
+'''
+
 ft = input('Enter the number of square feet in the tract: ')
-
 result = float(ft) / float(43560)
-
 print('The size of that tract is ', round(result, 2), ' acres.')
-
