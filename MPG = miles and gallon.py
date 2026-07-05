edf56a747e10eada7e236e3df765b7644a54be2a
https-Myrtle <myrtleargote5@gmail.com>
2026-07-06 05:20:13 +0800

    Update MPG = miles and gallon.py

--- MPG = miles and gallon.py
+++ MPG = miles and gallon.py
@@ -1,7 +1,18 @@
+#Author Myrtle Abigail
+#Date 07/04/2026
+
+'''
+Converting miles driven to gallon used
+
+input enter miles driven
+input gallons of uel used
+
+MPG = miles driven / gallon
+
+display the result of usafes of miles used per gallon.
+'''
+
 miles = input('Enter the miles driven:')
-
 gallon = input('Enter the gallons of fuel used:')
-
 result = float(miles) / float(gallon)
-
 print('You used ', round(result, 2), 'miles per gallon. ')
