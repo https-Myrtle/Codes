190aca03a61e8bc7b0e15a617bc97cc7d42c9c8d
https-Myrtle <myrtleargote5@gmail.com>
2026-07-11 18:03:04 +0800

    Update Kilometers to Miles.py

--- Kilometers to Miles.py
+++ Kilometers to Miles.py
@@ -1,14 +1,11 @@
-#Author Myrtle Abigail
+#Author Myrtle 
 #Date 6/25/2026
-
 '''
 km to m
 input km
 km * 0.621
 print answer
 '''
-
-
 value = float(input('Enter a value in kilometres: '))
 result = float(value)* 0.621 
 print(round(value, 2), 'km = ', round(result, 2), 'mi')
