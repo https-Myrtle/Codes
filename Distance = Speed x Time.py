d88bd6b6a7b684ca2f31a47668b6b7057e131b0f
https-Myrtle <myrtleargote5@gmail.com>
2026-07-11 18:02:49 +0800

    Update Distance = Speed x Time.py

--- Distance = Speed x Time.py
+++ Distance = Speed x Time.py
@@ -1,16 +1,12 @@
-#Author Myrtle Abigail
+#Author Myrtle
 #Date 07/03/2026
-
 '''
 input cars speed is
 input the time
-
 distance = speed * time
 Display car travelled then round it ny 2
 '''
-
 speed = input('Enter the cars speed: ')
 time = input('Enter the time: ')
 distance = float(speed) * float(time)
 print('The car travelled ', round(distance, 2))
-
