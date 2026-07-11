ad6ba5f4dd5ee85d14efddfdaf26e89ecfb5f5cf
https-Myrtle <myrtleargote5@gmail.com>
2026-07-11 18:06:38 +0800

    Update Numbers to Roman Numericals.py

--- Numbers to Roman Numericals.py
+++ Numbers to Roman Numericals.py
@@ -1,47 +1,32 @@
 #Author Myrtle 
 #Date 07/06/2026
-
 '''
 input "Enter an integer from 1 - 10
-
 if range is 1 - 10:
     display roman num
-
 else:
     print(Error:Invalid Number)
 '''
-
 value = int(input('Enter an integer from 1 - 10: '))
-
 if value == 1:
     print('I')
-
 elif value == 2:
     print('II')
-    
 elif value == 3:
     print('III')
-
 elif value == 4:
     print('IV')
-    
 elif value == 5:
     print('V')
-
 elif value == 6:
-    print('VI')
-    
+    print('VI') 
 elif value == 7:
-    print('VII')
-    
+    print('VII')  
 elif value == 8:
-    print('VIII')
-    
+    print('VIII') 
 elif value == 9:
-    print('IX')
-    
+    print('IX') 
 elif value == 10:
     print('X')
-
 else:
     print('Error: Invalid Number')
